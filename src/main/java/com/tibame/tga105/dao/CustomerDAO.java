a0f2183c96e2772b92ab95597e790c4ea5012e9b
Ryan <tototwgod5@gmail.com>
2023-03-06 10:02:20 +0800

    建好套件分支

--- src/main/java/com/tibame/tga105/dao/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.tibame.tga105.dao;
-
-import com.tibame.tga105.domain.CustomerBean;
-
-public interface CustomerDAO {
-	public abstract CustomerBean select(String custid);
-	public abstract boolean update(byte[] password, String email,
-			java.util.Date birth, String custid);
-}
